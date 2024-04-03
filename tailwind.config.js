35a6a68cef22f5ed1a87d0f54b531fdbe0d2eddd
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-04-03 14:13:28 +0000

    6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,15 @@
 /** TAILWIND - Define customizations at the root of the project */
 /** @type {import('tailwindcss').Config} */
 export default __CONFIG__
+
+/** TAILWIND - Add Tailwind Typography plugin */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      // ...
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+      // ...
+    ],
+  }
