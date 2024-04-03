3ae7710de8c088c5988f3d8a050b6c3d98aa77aa
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-04-03 14:10:30 +0000

    3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,3 @@
 /** TAILWIND - Define customizations at the root of the project */
-import('tailwindcss').Config
-module.exports = {
-    content: [
-        './src/**/*.{html,js}'],
-    theme: {
-      colors: {
-        'navy': '#112A46',
-        'sky-blue': '#ACC8E5',
-        'black': '#000000',
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      extend: {
-        spacing: {
-          '8xl': '96rem',
-          '9xl': '128rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
-    },
-  }
+/** @type {import('tailwindcss').Config} */
+export default __CONFIG__
